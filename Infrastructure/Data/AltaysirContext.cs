7e64831e2a57b5ab33e6cf31c487786f3d2988f7
abdulrahman <abdulrahmn398@outlook.com>
2025-11-08 21:39:56 +0200

    init commit

---
+++ Infrastructure/Data/AltaysirContext.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entities;
+
+namespace Infrastructure.Data;
+
+public class AltaysirContext : DbContext
+{
+    public AltaysirContext(DbContextOptions<AltaysirContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+}
